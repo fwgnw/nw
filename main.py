6e9f48f495e6c5cc57218ba318533d28096e3e7f
pi <pi@raspberrypi.(none)>
2014-11-29 12:52:25 +0000

    now works with 4 sensors

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
+
+TRIG = [16, 38, 35, 13]
+ECHO = [18, 36, 37, 11]
+RESULT = [0, 0, 0, 0]
+    
+
 def setup():
     GPIO.cleanup()
 
     GPIO.setmode(GPIO.BOARD)
-
-    TRIG = [16, 38, 35, 13]
-    ECHO = [18, 36, 37, 11]
-    RESULT = [0, 0, 0, 0]
 
     for trig in TRIG:
         GPIO.setup(trig, GPIO.OUT)
@@ -18,39 +20,44 @@ def setup():
         GPIO.setup(echo, GPIO.IN)
 
 
-def measure(i)
+def measure(i):
     GPIO.output(TRIG[i], True)
     time.sleep(0.00001)
     GPIO.output(TRIG[i], False)
 
-    while GPIO.input(ECHO[i]) == 0:
+    msr_start = time.time()
+
+    while GPIO.input(ECHO[i]) == 0 and time.time() - msr_start < 0.05:
         pulse_start = time.time()
 
-    while GPIO.input(ECHO[i]) == 1:
+    while GPIO.input(ECHO[i]) == 1 and time.time() - msr_start < 0.05:
         pulse_end = time.time()
 
-    RESULT[i] = pulse_end - pulse_start
+    if time.time() - msr_start > 0.05:
+        RESULT[i] = -1
+    else:
+        RESULT[i] = pulse_end - pulse_start
+    
+    time.sleep(.25)
 
 
 def print_result(i):
     distance = RESULT[i] * 17150
     distance = round(distance, 2)
 
-    print("distance = " + str(distance) + "cm")
+    print("distance[" + str(i) + "] = " + str(distance) + " cm")
 
 
 setup()
 
 while True:
-    start_prgrm = time.time()
-
-    # time.sleep(.5)
+    prgrm_start = time.time()
 
     for i in range(len(TRIG)):
         measure(i)
         print_result(i)
         
-    end_prgrm = time.time()
-    print("duration = " + str(round(end_prgrm - start_prgrm, 2)))
+    prgrm_end = time.time()
+    print("duration = " + str(round(prgrm_end - prgrm_start, 2)) + " s")
 
 GPIO.cleanup()

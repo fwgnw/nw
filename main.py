59b3bb229a82eafe91182d689606c982ce707d11
root <zoova@zoovali.net>
2015-01-27 16:08:54 +0100

    switched engine and steering

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ RESULT = [0, 0, 0, 0]
 DATA = [[], [], [], []]  #0: front, 1: left, 2: back, 3: right
 WDATA = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
-MOTOR = [32, 33, 31, 29]  #0: engine+, 1: engine-, 2: steering+, 3: steering-
+MOTOR = [31, 29, 32, 33]  #0: engine+, 1: engine-, 2: steering+, 3: steering-
 
 LOGFILE = "log/main_" + str(int(time.time())) + ".log"
 
